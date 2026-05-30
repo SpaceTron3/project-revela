7dfd12409b7a2449d34d2ed221fb7b0a71f5ce28
Victor Campos <victor@projectrevela.org>
2026-05-30 17:04:01 -0400

    Allow all image domains in next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['*'],
+    remotePatterns: [
+      { protocol: 'https', hostname: '**' },
+    ],
+  },
+}
+
 module.exports = nextConfig
